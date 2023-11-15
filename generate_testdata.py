805603db70d6f7137ba807ea575e5da89cd1fccd
Adam <48681209+add33m@users.noreply.github.com>
2023-11-15 18:11:16 +0100

    Completed test generation and test execution files

--- generate_testdata.py
+++ generate_testdata.py
@@ -4,7 +4,21 @@ import random as r
 # - different sizes
 # - different levels of sortedness
 
-def generate_testdata(size, sortedness):
+def insertionSort_special(arr, cmp):
+    n = len(arr)  # Get the length of the array
+      
+    if n <= 1:
+        return  # If the array has 0 or 1 element, it is already sorted, so return
+ 
+    for i in range(1, n):  # Iterate over the array starting from the second element
+        key = arr[i]  # Store the current element as the key to be inserted in the right position
+        j = i-1
+        while j >= 0 and (cmp(key, arr[j]) < 0):  # Move elements greater than key one position ahead
+            arr[j+1] = arr[j]  # Shift elements to the right
+            j -= 1
+        arr[j+1] = key  # Insert the key in the correct position
+
+def generate_testdata(size, randomness):
   data = []
   i = 0
   while i < size:
@@ -13,7 +27,7 @@ def generate_testdata(size, sortedness):
 
   def comparator(a, b):
     flip = 1
-    if r.random < sortedness/2:
+    if r.random() < randomness/2:
       flip = -1
 
     if a < b:
@@ -23,8 +37,38 @@ def generate_testdata(size, sortedness):
     else:
       return 0
 
-  data = sorted(data, cmp)
+  if randomness != 1:
+    insertionSort_special(data, comparator)
 
   return data
 
-print(generate_testdata(10**1, .1))
+# Example data to show different sortedness levels
+# print("Unsorted:")
+# print(generate_testdata(3*10**1, 1))
+# print("Mostly unsorted:")
+# print(generate_testdata(3*10**1, .1))
+# print("Almost sorted:")
+# print(generate_testdata(3*10**1, .02))
+
+def generate_dataset():
+  print("Generating dataset...")
+  with open("dataset", "w") as f:
+    # Generate 24 datasets of completely unsorted random data, sizes 2^3 - 2^24
+    print("Generating unsorted data...")
+    for size in range(3, 25):
+        f.write(str(generate_testdata(2**size, 1)))
+        f.write("\n")
+
+    # Generate 24 datasets of mostly unsorted random data, sizes 2^3 - 2^24
+    print("Generating mostly unsorted data...")
+    for size in range(3, 25):
+        f.write(str(generate_testdata(2**size, .1)))
+        f.write("\n")
+
+    # Generate 24 datasets of almost sorted random data, sizes 2^3 - 2^24
+    print("Generating almost sorted data...")
+    for size in range(3, 25):
+        f.write(str(generate_testdata(2**size, .02)))
+        f.write("\n")
+
+generate_dataset()
--- test_algorithm.py
+++ test_algorithm.py
@@ -8,23 +8,67 @@ def run_test(algo, data):
   ops_count = algo(data)
   time_delta = t.timeit(lambda: algo(data), number=1)
 
-  i = 0
+  # Check so sorted array actually is sorted correctly
   result_is_correct = True
-  while i < len(data)-1:
-    if data[i] > data[i+1]:
-      result_is_correct = False
-      break
-    i += 1
+  # i = 0
+  # while i < len(data)-1:
+  #   if data[i] > data[i+1]:
+  #     result_is_correct = False
+  #     break
+  #   i += 1
 
   return ops_count, time_delta, result_is_correct
 
 
 
 # Example test run
-test = []
-i = 0
-while i < 2*10**4:
-  i += 1
-  test.append(r.randint(0, 10**8))
-print("Running test")
-print(run_test(bSortCount, test))
+# test = []
+# i = 0
+# while i < 2*10**4:
+#   i += 1
+#   test.append(r.randint(0, 10**8))
+# print(run_test(bSortCount, test))
+
+def run_testset(algo):
+  print("Running test...")
+  with open("dataset", "r") as f:
+    # Generate 24 datasets of completely unsorted random data, sizes 2^3 - 2^24
+    print("Testing unsorted data...")
+    for size in range(3, 25):
+      linedata = f.readline().split(", ")
+      # Remove leading "'["
+      linedata[0] = linedata[0][2:]
+
+      # Remove trailing "]\n'"
+      linedata[-1] = linedata[-1][:-2]
+
+      if size < 10:
+        print("Size = ", 2**size, run_test(algo, linedata))
+
+    # Generate 24 datasets of mostly unsorted random data, sizes 2^3 - 2^24
+    print("Generating mostly unsorted data...")
+    for size in range(3, 25):
+      linedata = f.readline().split(", ")
+      # Remove leading "'["
+      linedata[0] = linedata[0][2:]
+
+      # Remove trailing "]\n'"
+      linedata[-1] = linedata[-1][:-2]
+
+      if size < 10:
+        print("Size = ", 2**size, run_test(algo, linedata))
+
+    # Generate 24 datasets of almost sorted random data, sizes 2^3 - 2^24
+    print("Generating almost sorted data...")
+    for size in range(3, 25):
+      linedata = f.readline().split(", ")
+      # Remove leading "'["
+      linedata[0] = linedata[0][2:]
+
+      # Remove trailing "]\n'"
+      linedata[-1] = linedata[-1][:-2]
+
+      if size < 10:
+        print("Size = ", 2**size, run_test(algo, linedata))
+
+run_testset(bSortCount)
