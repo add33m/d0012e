c409ecc0bde6ce892d94440ce7a5f379e2ac9389
add33m <48681209+add33m@users.noreply.github.com>
2023-11-16 17:06:35 +0100

    Added more tests and output to excel

--- mergeSort2.py
+++ mergeSort2.py
@@ -1,3 +1,4 @@
+from bSort import bSortCount
 
 def insertionSort2(arr):
     n = len(arr)  # Get the length of the array
@@ -11,7 +12,7 @@ def insertionSort2(arr):
         arr[j+1] = key  # Insert the key in the correct position
     return arr
 
-def megesort(list, k):
+def mergesort2(list, k):
 
     listlist = []
 
@@ -62,8 +63,114 @@ def merge(list1, list2):
     # returns the merged list
     return l
 
+############################################################
+# Versions of above code with iteration counters included: #
+############################################################
+
+def insertionSort2Count(arr):
+    counter = 1
+    n = len(arr)  # Get the length of the array
+      
+    for i in range(1, n):  # Iterate over the array starting from the second element
+        counter += 1
+        key = arr[i]  # Store the current element as the key to be inserted in the right position
+        j = i-1
+        while j >= 0 and key < arr[j]:  # Move elements greater than key one position ahead
+            counter += 1
+            arr[j+1] = arr[j]  # Shift elements to the right
+            j -= 1
+        arr[j+1] = key  # Insert the key in the correct position
+    return counter
+
+def mergesort2CountI(list, k):
+    counter = 1
+    listlist = []
+
+    # splits the list up in sublists, sorts the sublist with insertion or bsort and appends each sublist to the list of lists "listlist".
+    for i in range(0, len(list), k):
+        counter += 1
+        n = len(list)
+        if n <= 1:
+            listlist.append[i:i + k]
+        else:
+            tempList = list[i:i + k]
+            subcount = insertionSort2Count(tempList)
+            counter += subcount
+            listlist.append(tempList)
+
+    # outer while loop that repeats the inner merge loop until the list of lists only contains one compleate merged list.
+    while (len(listlist) > 1):
+        counter += 1
+    # inner while loop that merges two lists (list1 and list2) inside the list-list and then returns the merged list into the list-list
+        i = 0
+        while i < (len(listlist)-1):
+            counter += 1
+            listlist[i], subcount = mergeCount(listlist[i], listlist[i+1])
+            counter += subcount
+            listlist.pop(i+1)
+            i += 1
+    # returns the first and only list left inside the list of lists, that should now contain all sorted values
+    return listlist[0], counter
+
+def mergesort2CountB(list, k):
+    counter = 1
+    listlist = []
+
+    # splits the list up in sublists, sorts the sublist with insertion or bsort and appends each sublist to the list of lists "listlist".
+    for i in range(0, len(list), k):
+        counter += 1
+        n = len(list)
+        if n <= 1:
+            listlist.append[i:i + k]
+        else:
+            tempList = list[i:i + k]
+            subcount = bSortCount(tempList)
+            counter += subcount
+            listlist.append(tempList)
+
+    # outer while loop that repeats the inner merge loop until the list of lists only contains one compleate merged list.
+    while (len(listlist) > 1):
+        counter += 1
+    # inner while loop that merges two lists (list1 and list2) inside the list-list and then returns the merged list into the list-list
+        i = 0
+        while i < (len(listlist)-1):
+            counter += 1
+            listlist[i], subcount = mergeCount(listlist[i], listlist[i+1])
+            counter += subcount
+            listlist.pop(i+1)
+            i += 1
+    # returns the first and only list left inside the list of lists, that should now contain all sorted values
+    return listlist[0], counter
+
+    #the merge function that uses standard merge-sort technique to merge two sorted lists into one sorted list
+def mergeCount(list1, list2):
+    counter = 1
+
+    l = []
+    i = 0
+    j = 0
+  
+    # runs loop until we have reached the end of either list 1 or list 2
+    while i < len(list1) and j < len(list2):
+        counter += 1
+    # if the smallest element left in list 1 is smaller than the smallest element left in list 2, append it into the merged list.
+        if list1[i] < list2[j]:
+            l.append(list1[i])
+            i += 1
+    # if the smallest element left in list 2 is smaller than the smallest element left in list 1, append it into the merged list.
+        else:
+            l.append(list2[j])
+            j += 1
+
+    # append the rest of the numbers from the list that still has numbers left inside of it after te while loop has compleated.
+    l += list1[i:]
+    l += list2[j:]
+
+    # returns the merged list
+    return l, counter
+
 newList = [5,3,1,2,6,4,9,7,8,10,61,55,33,45,86,32,14,57,87,99,55,30,22,66,88]
 
 print(newList)
 
-print(megesort(newList, 3))
+print(mergesort2(newList, 3))
--- test_algorithm.py
+++ test_algorithm.py
@@ -2,22 +2,18 @@ import timeit as t
 import random as r
 
 from bSort import bSortCount
+from mergeSort2 import mergesort2CountI, mergesort2CountB
 
 # Run a test given an algorithm and input data, returns "operation count", time taken (secs) and if data is correct
-def run_test(algo, data):
-  ops_count = algo(data)
-  time_delta = t.timeit(lambda: algo(data), number=1)
+def run_test(algo, data, k):
+  ops_count = [0]
 
-  # Check so sorted array actually is sorted correctly
-  result_is_correct = True
-  # i = 0
-  # while i < len(data)-1:
-  #   if data[i] > data[i+1]:
-  #     result_is_correct = False
-  #     break
-  #   i += 1
+  def test():
+    ops_count[0] = algo(data, k)[1]
 
-  return ops_count, time_delta, result_is_correct
+  time_delta = t.timeit(test, number=1)
+
+  return ops_count[0], time_delta
 
 
 
@@ -29,46 +25,66 @@ def run_test(algo, data):
 #   test.append(r.randint(0, 10**8))
 # print(run_test(bSortCount, test))
 
-def run_testset(algo):
-  print("Running test...")
+def run_testset(algo, name):
+  print("Running test...", name)
   with open("dataset", "r") as f:
+    # Local func to run same code thrice
+    def _test(type):
+      # Open a file to write results to
+      with open(f"{name}_{type}.csv", "w") as csv:
+        # Write header for csv
+        csv.write("size, k2, k8, k32, k128, k256\n")
+
+        # Read all the sizes
+        for size in range(3, 25):
+          if size < 19:
+            linedata = f.readline().split(", ")
+            # Remove leading "'["
+            linedata[0] = linedata[0][2:]
+
+            # Remove trailing "]\n'"
+            linedata[-1] = linedata[-1][:-2]
+            line = str(2**size) + ","
+            line += str(run_test(algo, linedata, 2)[0]) + ","
+            line += str(run_test(algo, linedata, 8)[0]) + ","
+            line += str(run_test(algo, linedata, 32)[0]) + ","
+            line += str(run_test(algo, linedata, 128)[0]) + ","
+            line += str(run_test(algo, linedata, 256)[0]) + "\n"
+            print("Tested", name, type, 2**size)
+            print(line)
+            csv.write(line)
+
+            # print("Size = ", 2**size, "\tk = 2", run_test(algo, linedata, 2))
+            # print("Size = ", 2**size, "\tk = 8", run_test(algo, linedata, 8))
+            # print("Size = ", 2**size, "\tk = 32", run_test(algo, linedata, 32))
+            # print("Size = ", 2**size, "\tk = 128", run_test(algo, linedata, 128))
+            # print("Size = ", 2**size, "\tk = 256", run_test(algo, linedata, 256))
+
+            # Find optimal k for this dataset, size 12
+            # if size == 12:
+            #   k = 0
+              
+            #   while k < 2**size:
+            #     k += 1
+            #     run_test()
+
+          else:
+            f.readline()
+
     # Generate 24 datasets of completely unsorted random data, sizes 2^3 - 2^24
     print("Testing unsorted data...")
-    for size in range(3, 25):
-      linedata = f.readline().split(", ")
-      # Remove leading "'["
-      linedata[0] = linedata[0][2:]
-
-      # Remove trailing "]\n'"
-      linedata[-1] = linedata[-1][:-2]
-
-      if size < 10:
-        print("Size = ", 2**size, run_test(algo, linedata))
+    _test("us")
 
     # Generate 24 datasets of mostly unsorted random data, sizes 2^3 - 2^24
-    print("Generating mostly unsorted data...")
-    for size in range(3, 25):
-      linedata = f.readline().split(", ")
-      # Remove leading "'["
-      linedata[0] = linedata[0][2:]
-
-      # Remove trailing "]\n'"
-      linedata[-1] = linedata[-1][:-2]
-
-      if size < 10:
-        print("Size = ", 2**size, run_test(algo, linedata))
+    print("Testing mostly unsorted data...")
+    _test("mus")
 
     # Generate 24 datasets of almost sorted random data, sizes 2^3 - 2^24
-    print("Generating almost sorted data...")
-    for size in range(3, 25):
-      linedata = f.readline().split(", ")
-      # Remove leading "'["
-      linedata[0] = linedata[0][2:]
+    print("Testing almost sorted data...")
+    _test("as")
 
-      # Remove trailing "]\n'"
-      linedata[-1] = linedata[-1][:-2]
-
-      if size < 10:
-        print("Size = ", 2**size, run_test(algo, linedata))
-
-run_testset(bSortCount)
+# run_testset(bSortCount)
+print("I")
+run_testset(mergesort2CountI, "ins")
+print("B")
+run_testset(mergesort2CountB, "bin")
