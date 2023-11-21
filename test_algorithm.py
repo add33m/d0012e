11f1b65e45e6b79ad5dde9305b9fb473fceeecc0
add33m <48681209+add33m@users.noreply.github.com>
2023-11-21 22:44:18 +0100

    Added code to create csv comparing k-values

--- test_algorithm.py
+++ test_algorithm.py
@@ -98,7 +98,7 @@ def optimize_k(algo, name):
     def _test(type):
       # Read all the sizes
       for size in range(3, 25):
-        if 5 < size < 16:
+        if 9 < size < 11:
           linedata = f.readline().split(", ")
           # Remove leading "'["
           linedata[0] = linedata[0][2:]
@@ -107,25 +107,49 @@ def optimize_k(algo, name):
           linedata[-1] = linedata[-1][:-2]
           
           # Divide-and-conquer to find optimal k by looking in the half where delta between border and middle point is lowest
-          upper_bound = 2**(size-1)
-          lower_bound = 1
-          upper = run_test(algo, linedata, upper_bound, 5)[1]
-          lower = run_test(algo, linedata, lower_bound, 5)[1]
-          middle = run_test(algo, linedata, (upper_bound+lower_bound)//2, 5)[1]
+          # upper_bound = 2**(size-1)
+          # lower_bound = 1
+          # upper = run_test(algo, linedata, upper_bound, 5)[1]
+          # lower = run_test(algo, linedata, lower_bound, 5)[1]
+          # middle = run_test(algo, linedata, (upper_bound+lower_bound)//2, 5)[1]
 
-          while upper_bound - lower_bound > 1:
-            if abs(upper-middle) < abs(lower - middle):
-              # Choose upper half
-              lower = middle
-              lower_bound = (upper_bound+lower_bound) // 2
-            else:
-              # Choose lower half
-              upper = middle
-              upper_bound = (upper_bound+lower_bound) // 2
+          # while upper_bound - lower_bound > 1:
+          #   if abs(upper-middle) < abs(lower - middle):
+          #     # Choose upper half
+          #     lower = middle
+          #     lower_bound = (upper_bound+lower_bound) // 2
+          #   else:
+          #     # Choose lower half
+          #     upper = middle
+          #     upper_bound = (upper_bound+lower_bound) // 2
 
-            middle = run_test(algo, linedata, (upper_bound+lower_bound)//2, 5)[1]
+          #   middle = run_test(algo, linedata, (upper_bound+lower_bound)//2, 5)[1]
+
+          # For loop based k optimizer
+          # k = 1
+          # bestK = 1
+          # bestTime = 9999999999
+          # while True:
+          #   currentTime = run_test(algo, linedata, k, 100)[1]
+          #   if currentTime < bestTime:
+          #     bestTime = currentTime
+          #     bestK = k
+          #   elif currentTime > bestTime * 1.06:
+          #     break
+
+          #   k += 1
             
-          print(f"Optimal k for size {2**size} =", lower_bound, "with time", lower)
+          # print(f"Optimal k for size {2**size} =", bestK, "with time", bestTime, ", tested up to k =", k)
+
+          # Create csv with values
+          with open(f"{name}_k_optimize.csv", "w") as csv:
+            csv.write("k,time\n")
+            k = 1
+            while k <= 2**size:
+              time_taken = run_test(algo, linedata, k, 10)[1]
+              csv.write(f"{k},{time_taken}\n")
+              k += 1
+
         else:
           f.readline()
 
@@ -134,12 +158,12 @@ def optimize_k(algo, name):
     _test("us")
 
     # Generate 24 datasets of mostly unsorted random data, sizes 2^3 - 2^24
-    print("Testing mostly unsorted data...")
-    _test("mus")
+    # print("Testing mostly unsorted data...")
+    # _test("mus")
 
     # Generate 24 datasets of almost sorted random data, sizes 2^3 - 2^24
-    print("Testing almost sorted data...")
-    _test("as")
+    # print("Testing almost sorted data...")
+    # _test("as")
 
 print("I")
 optimize_k(mergesort2CountI, "ins")
