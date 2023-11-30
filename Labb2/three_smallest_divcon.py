9374560808d078e1a7eea4a3eef447dbaa9dd0af
Adam <48681209+add33m@users.noreply.github.com>
2023-11-30 18:30:50 +0100

    Added correct calculations for comparison numbers

--- Labb2/three_smallest_divcon.py
+++ Labb2/three_smallest_divcon.py
@@ -3,9 +3,16 @@
 # With this we can divide in 2 until array is exactly len 3, and return these values in sorted order
 # We then perform use a mergesort-like algorithm and always return only the three smallest values
 
-# This algorithm will not run in exactly O(n) but worst-case is approximately linear O(n + log n) ~ O(n)
+# This algorithm will not run in exactly O(n) but worst-case is approximately linear O(3n + 3 * log n) ~ O(n)
 # Exact number of comparisons will be the following:
 #   n [for bottom-level insertionsort] + 3 * (k-1) [for mergesort]
+
+# n = 3 * 2^(k-1)
+# log2(n / 3) + 1 = k
+
+# k = 4
+# 3 * 2^3 = 3*8 = 24
+# 24 + 3*7 = 24 + 21
 
 # Proof (induction):
 # 1. Base case: k = 1 <=> n = 3
@@ -21,7 +28,25 @@
 # This will produce a sorted list of 3 elements
 # Therefore, the algo will produce a sorted list of 3 elements for all (natural) values of k >= 1
 
+# n = 3 => 4 = 4
+# n = 6 => 1 + 4 + 4 + 3 = 12
+# n = 12 => 1 + 1 + 1 + 4 + 4 + 4 + 4 + 3 + 3 + 3 = 28
+# n = 24 => 1 + 1 + 1 + 1 + 1 + 1 + 1 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 3 + 3 + 3 + 3 + 3 + 3 + 3 = 60
+# n = 3 * 2^(k-1) => (2^k-1)*1 + 2^k*4 + (2^k-1)*3 = 2^k*4 + (2^k-1)*4 = 4*(2*2^k-1) = 2^(k+2) - 4
+# C(k) = 2^(k+2) - 4
+# k = log2(n / 3) + 1
+# C(n) = 2^(log2(n / 3) + 1 + 2) - 4
+# C(n) = 2^(log2(n / 3) + 3) - 4
+# C(n) = 2^(log2(n/3))*2^3 - 4
+# C(n) = (8/3)*n - 4
+
+       #
+   #       #
+ #   #   #   #
+# # # # # # # #
+
 def three_smallest_divcon(list):
+    counter = 4
     if len(list) == 3:
         # Hardcoded insertionsort for n = 3
         x, y, z = list
@@ -32,12 +57,12 @@ def three_smallest_divcon(list):
         if y < x:
             x, y = y, x
     
-        return x, y, z
+        return (x, y, z), counter
         
 
     right_of_center = len(list) // 2
-    smallest_in_left_half = three_smallest_divcon(list[:right_of_center])
-    smallest_in_right_half = three_smallest_divcon(list[right_of_center:])
+    smallest_in_left_half, l_counter = three_smallest_divcon(list[:right_of_center])
+    smallest_in_right_half, r_counter = three_smallest_divcon(list[right_of_center:])
 
     # Perform a simple mergesort of the 6 values we have
     left_counter = 0
@@ -52,7 +77,7 @@ def three_smallest_divcon(list):
             right_counter += 1
     
     # Return output converted to tuple
-    return tuple(output)
+    return tuple(output), counter + l_counter + r_counter
 
 # testlist = [16, 49, 22, 38, 14, 38, 50, 8, 25, 13, 39, 40, 36, 22, 27, 44, 23, 32, 25, 15, 47, 30, 48, 48]
 # print(three_smallest_divcon(testlist))
