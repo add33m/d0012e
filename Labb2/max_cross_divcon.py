7740a423249931341da8795175932e2c1e55d952
add33m <48681209+add33m@users.noreply.github.com>
2023-12-05 17:29:41 +0100

    Implemented working MSAS div/con algo

---
+++ Labb2/max_cross_divcon.py
@@ -0,0 +1,36 @@
+
+def max_cross_divcon(list):
+  # Vals to calculate
+  # msas (Maximum SubArray Sum), msas_l (msas that touches left edge), msas_r (msas that touches right edge), tot
+
+  if len(list) == 2:
+    # This is just a simpler case of the same calculations as below
+    msas = max(list[0], list[1], list[0] + list[1])
+    msas_l = max(list[0], list[0] + list[1])
+    msas_r = max(list[1], list[0] + list[1])
+    tot = list[0] + list[1]
+
+    return msas, msas_l, msas_r, tot
+
+  # Generate all lower vals for left and right halfs recursively
+  mid = len(list) // 2
+  left_msas, left_msas_l, left_msas_r, left_tot = max_cross_divcon(list[:mid])
+  right_msas, right_msas_l, right_msas_r, right_tot = max_cross_divcon(list[mid:])
+
+  # total MSAS is either left MSAS or right MSAS or total of both lower MSASes touching the middle
+  # This is because the msas will either be only in the left half or only the right half or cross the middle, there are no other possibilities 
+  msas = max(left_msas, right_msas, left_msas_r + right_msas_l)
+  # MSAS touching the left edge is either only left half's left edge MSAS or ALL of the left half + right half's left edge MSAS
+  msas_l = max(left_msas_l, left_tot + right_msas_l)
+  # Same respectively for right edge msas
+  msas_r = max(right_msas_r, right_tot + left_msas_r)
+  # Total is just sum of both halves' totals
+  tot = left_tot + right_tot
+
+  return msas, msas_l, msas_r, tot
+
+test = [-1, -1, -1, 10, -5, 10, -1, -1]
+test1 = [16, -17, 6, -2, 19, -18, 4, 1, 15, -1, -1, -1, -1, -1, -1, 1]
+test2 = [1, -3, 79, 5, -1, 80, 2, 9]
+
+print("MSAS:", max_cross_divcon(test)[0])
